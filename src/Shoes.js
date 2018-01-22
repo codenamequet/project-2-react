b302d0bb96bdf3f47cf1b2c8795e1c1c0ff58c20
Your Name <you@example.com>
2018-01-22 18:00:09 -0500

    downloaded a few dependencies and made shoes.js file

---
+++ src/Shoes.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class Shoes extends Component {
+    state = {
+        name: [],
+        img: [],
+    }
+    
+    
+    
+    
+    
+    render () {
+        return (
+            <div>
+                <p>Shoes are here!</p>
+            </div>
+        )
+    }
+}
+
+export default Shoes
