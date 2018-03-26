472859af60cbd128498427b6e49a29a655441832
Marquet Reid <you@example.com>
2018-03-26 04:38:17 -0400

    cleaning up Shoes component

--- src/components/Shoes.js
+++ src/components/Shoes.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import { SHOES_URL } from '../constants.js'
-// import './Shoes.css'
 
 const shoesStyle = {
     color: 'blue',
-    // width: '200px',
-    // height: '250px',
     width: '15vw',
     height: '25vh',
 }
@@ -20,20 +17,16 @@ class Shoes extends Component {
         axios
         .get(`${SHOES_URL}`)
         .then(response => this.setState({shoes: response.data.shoes}))
-        // .then(response => {console.log(response.data)})
-        // .then(response => console.log(response))
         .catch(err => console.log(err))
-        // console.log(this.state.shoes)
+
         }
     
     render () {
         let shoes = this.state.shoes
-        // console.log('shoes is now ', shoes)
         console.log('shoes[0] is ', shoes)
         console.log('shoes[0].shoes is ', shoes)
         return (
             <div>
-                {/* <p>{shoes.length && shoes[0].name}</p> */}
                 <img style={shoesStyle} src={shoes.length && shoes[0].images} />
                 <img style={shoesStyle} src={shoes.length && shoes[1].images} />
                 <img style={shoesStyle} src={shoes.length && shoes[2].images} />
